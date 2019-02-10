afe407292ec7725ddb23401df1cb824231b10068
AlexSheer <alex_sheer@inbox.lv>
2019-02-10 23:01:52 +0200

    Add files via upload

---
+++ migrations/img_to_attach_1_0_1.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+* @package phpBB Extension - IMG to Attachment
+* @copyright (c) 2015 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+namespace sheer\img_to_attach\migrations;
+
+class img_to_attach_1_0_1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['img_to_attach_version']) && version_compare($this->config['img_to_attach_version'], '1.0.1', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\sheer\img_to_attach\migrations\img_to_attach_1_0_0');
+	}
+
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Current version
+			array('config.update', array('img_to_attach_version', '1.0.1')),
+		);
+	}
+}
